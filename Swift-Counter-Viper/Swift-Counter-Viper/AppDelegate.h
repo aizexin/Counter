410a0ee3088fee50bfbaa4b53217cdd9fc02a415
艾泽鑫 <443792431@qq.com>
2018-09-27 10:51:03 +0800

    init commit

---
+++ Swift-Counter-Viper/Swift-Counter-Viper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swift-Counter-Viper
+//
+//  Created by 艾泽鑫 on 2018/9/26.
+//  Copyright © 2018 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swift-Counter-Viper/Swift-Counter-Viper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Swift-Counter-Viper
+//
+//  Created by 艾泽鑫 on 2018/9/26.
+//  Copyright © 2018 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
