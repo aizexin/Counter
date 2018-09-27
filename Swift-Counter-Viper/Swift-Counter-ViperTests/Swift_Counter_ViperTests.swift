e42222ce5063714a000074b8ef75caa925a25a5c
艾泽鑫 <443792431@qq.com>
2018-09-27 10:58:16 +0800

    改为swift项目

---
+++ Swift-Counter-Viper/Swift-Counter-ViperTests/Swift_Counter_ViperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_Counter_ViperTests.swift
+//  Swift-Counter-ViperTests
+//
+//  Created by 艾泽鑫 on 2018/9/27.
+//  Copyright © 2018 艾泽鑫. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Counter_Viper
+
+class Swift_Counter_ViperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
